0e0f9b33250260af19af8f0c9d8db187b56a50b5
wwx_glb <10086@wwx.com>
2022-06-17 23:39:21 +0800

    自动部署

---
+++ cname.sh
@@ -0,0 +1,7 @@
+# cname.sh
+
+#!/usr/bin/env sh
+
+set -e
+
+echo 'hihixixi.github.io/study-notes/' > docs/.vuepress/dist/CNAME
---
+++ docs/.vuepress/config/secret.js
@@ -0,0 +1,4 @@
+module.exports = {
+    clientId: '7ec6e50f0194e6864c74',
+    clientSecret: '1a3bb85eb496638363693a614c7f306787e9113e',
+}
